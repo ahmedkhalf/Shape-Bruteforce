3bd33274ba39e5b4a0a08b25eb93d93a7191bfd6
Ahmed <ahmedkhalf567@gmail.com>
2020-07-26 22:05:49 +0400

    Use argparse

--- shape_bruteforce/main.py
+++ shape_bruteforce/main.py
@@ -1,5 +1,13 @@
+import argparse
+from shape_bruteforce import utils
+
+
 def main():
-    pass
+    """Entrypoint of application"""
+    version = utils.get_version()
+    parser = argparse.ArgumentParser(description=f"Shape Bruteforce CLI v{version}")
+    parser.add_argument("image", type=str, help="Image to be processed")
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
