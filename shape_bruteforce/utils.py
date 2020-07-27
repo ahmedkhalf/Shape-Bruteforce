828a15263f37450d80ead0897af99be844f23195
Ahmed <ahmedkhalf567@gmail.com>
2020-07-27 23:53:49 +0400

    Add image resize

--- shape_bruteforce/utils.py
+++ shape_bruteforce/utils.py
@@ -3,6 +3,11 @@ from matplotlib import pyplot
 import os
 from shape_bruteforce import _version
 
+RESIZE_MAX = 0
+RESIZE_MIN = 1
+RESIZE_HEIGHT = 2
+RESIZE_WIDTH = 3
+
 
 def get_version():
     """Version info"""
@@ -14,6 +19,42 @@ def load_image(path):
         raise FileNotFoundError(f"Image was not found at {path}")
 
     img = cv2.imread(path)
+    return img
+
+
+def _resize_height(img, max_size):
+    height, width = img.shape[0:2]
+    scale_factor = max_size / height
+    width = int(width * scale_factor)
+    img = cv2.resize(img, (width, max_size), interpolation=cv2.INTER_AREA)
+    return img
+
+
+def _resize_width(img, max_size):
+    height, width = img.shape[0:2]
+    scale_factor = max_size / width
+    height = int(height * scale_factor)
+    img = cv2.resize(img, (max_size, height), interpolation=cv2.INTER_AREA)
+    return img
+
+
+def resize_image(img, max_size, mode=RESIZE_MIN):
+    height, width = img.shape[0:2]
+    if mode == RESIZE_MIN:
+        if height < width:
+            img = _resize_height(img, max_size)
+        else:
+            img = _resize_width(img, max_size)
+    elif mode == RESIZE_MAX:
+        if height > width:
+            img = _resize_height(img, max_size)
+        else:
+            img = _resize_width(img, max_size)
+    elif mode == RESIZE_HEIGHT:
+        img = _resize_height(img, max_size)
+    else:  # RESIZE_WIDTH
+        img = _resize_width(img, max_size)
+
     return img
 
 
