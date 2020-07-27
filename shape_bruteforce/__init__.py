ec8929e7da3cb2836c3125c5503f9fe2985b0ca9
Ahmed <ahmedkhalf567@gmail.com>
2020-07-27 18:59:42 +0400

    Add shapes

--- shape_bruteforce/__init__.py
+++ shape_bruteforce/__init__.py
@@ -1,1 +1,2 @@
-__version__ = "0.0.1"
+from shape_bruteforce import utils   # noqa: F401
+from shape_bruteforce import shapes  # noqa: F401
--- shape_bruteforce/main.py
+++ shape_bruteforce/main.py
@@ -1,16 +1,15 @@
 import argparse
-from shape_bruteforce import utils
+import shape_bruteforce as sb
 
 
 def main():
     """Entrypoint of application"""
-    version = utils.get_version()
+    version = sb.utils.get_version()
     parser = argparse.ArgumentParser(description=f"Shape Bruteforce CLI v{version}")
     parser.add_argument("image", type=str, help="Image to be processed")
     args = parser.parse_args()
 
-    img = utils.load_image(args.image)
-    utils.show_image(img)
+    target = sb.utils.load_image(args.image)
 
 
 if __name__ == "__main__":
