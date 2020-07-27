49ab0ab1517b8d1347cfcb653528ebc67838697b
Ahmed <ahmedkhalf567@gmail.com>
2020-07-28 00:29:50 +0400

    FIx bug that causes image to be upscaled

--- shape_bruteforce/utils.py
+++ shape_bruteforce/utils.py
@@ -24,6 +24,8 @@ def load_image(path):
 
 def _resize_height(img, max_size):
     height, width = img.shape[0:2]
+    if height < max_size:
+        return img
     scale_factor = max_size / height
     width = int(width * scale_factor)
     img = cv2.resize(img, (width, max_size), interpolation=cv2.INTER_AREA)
@@ -32,6 +34,8 @@ def _resize_height(img, max_size):
 
 def _resize_width(img, max_size):
     height, width = img.shape[0:2]
+    if width < max_size:
+        return img
     scale_factor = max_size / width
     height = int(height * scale_factor)
     img = cv2.resize(img, (max_size, height), interpolation=cv2.INTER_AREA)
