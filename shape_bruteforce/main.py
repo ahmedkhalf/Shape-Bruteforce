6b104bd549026450e907d39b09e0239c14a434fa
Ahmed <ahmedkhalf567@gmail.com>
2020-07-28 00:30:44 +0400

    Add --size argument

--- shape_bruteforce/main.py
+++ shape_bruteforce/main.py
@@ -7,9 +7,12 @@ def main():
     version = sb.utils.get_version()
     parser = argparse.ArgumentParser(description=f"Shape Bruteforce CLI v{version}")
     parser.add_argument("image", type=str, help="Image to be processed")
+    parser.add_argument("-s", "--size", default=128, type=int,
+                        help="Max size of image to be processed, smaller = faster")
     args = parser.parse_args()
 
     target = sb.utils.load_image(args.image)
+    target = sb.utils.resize_image(target, args.size)
 
 
 if __name__ == "__main__":
