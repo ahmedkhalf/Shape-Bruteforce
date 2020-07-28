249a491d2c53874a1b8f17930f003c503e788af1
Ahmed <ahmedkhalf567@gmail.com>
2020-07-28 20:27:49 +0400

    Add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,43 @@
+import shape_bruteforce as sb
+import unittest
+import pathlib
+
+
+class TestLoad(unittest.TestCase):
+    def setUp(self):
+        self.path = pathlib.Path(__file__).parent.joinpath("fixtures", "mona-lisa.jpg")
+        self.path = str(self.path)
+
+    def test_load(self):
+        sb.utils.load_image(self.path)
+
+    def test_no_load(self):
+        with self.assertRaises(FileNotFoundError):
+            sb.utils.load_image("ifejfijfjej6969.frok")
+
+
+class TestResize(unittest.TestCase):
+    def setUp(self):
+        self.path = pathlib.Path(__file__).parent.joinpath("fixtures", "mona-lisa.jpg")
+        self.path = str(self.path)
+        self.img = sb.utils.load_image(self.path)
+
+    def test_resize_height(self):
+        image = sb.utils.resize_image(self.img, 64, mode=sb.utils.RESIZE_HEIGHT)
+        self.assertEquals(image.shape[0], 64)
+
+    def test_resize_width(self):
+        image = sb.utils.resize_image(self.img, 64, mode=sb.utils.RESIZE_WIDTH)
+        self.assertEquals(image.shape[1], 64)
+
+    def test_resize_min(self):
+        image = sb.utils.resize_image(self.img, 64, mode=sb.utils.RESIZE_MIN)
+        self.assertEquals(image.shape[1], 64)
+
+    def test_resize_max(self):
+        image = sb.utils.resize_image(self.img, 64, mode=sb.utils.RESIZE_MAX)
+        self.assertEquals(image.shape[0], 64)
+
+
+if __name__ == '__main__':
+    unittest.main()
