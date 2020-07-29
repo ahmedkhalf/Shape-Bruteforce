d78961cbd38ef784071a713ca001d531e302333b
Ahmed <ahmedkhalf567@gmail.com>
2020-07-29 21:08:03 +0400

    Add easter egg

--- shape_bruteforce/main.py
+++ shape_bruteforce/main.py
@@ -2,14 +2,37 @@ import argparse
 import shape_bruteforce as sb
 
 
+def egg():
+    print("""
+        ████
+      ██░░░░██
+    ██░░░░░░░░██
+    ██░░░░░░░░██
+  ██░░░░░░░░░░░░██  Congratulations!
+  ██░░░░░░░░░░░░██  You are one of the few people who read my code.
+  ██░░░░░░░░░░░░██  Here is an egg.
+    ██░░░░░░░░██
+      ████████
+    """)
+
+
 def main():
     """Entrypoint of application"""
     version = sb.utils.get_version()
     parser = argparse.ArgumentParser(description=f"Shape Bruteforce CLI v{version}")
-    parser.add_argument("image", type=str, help="Image to be processed")
+    parser.add_argument("image", type=str, nargs='?', default="", help="Image to be processed")
     parser.add_argument("-s", "--size", default=128, type=int,
                         help="Max size of image to be processed, smaller = faster")
+    parser.add_argument("--egg", action='store_true', help=argparse.SUPPRESS)
     args = parser.parse_args()
+
+    if args.egg:
+        egg()
+        return
+
+    if args.image == "":
+        parser.print_help()
+        return
 
     target = sb.utils.load_image(args.image)
     target = sb.utils.resize_image(target, args.size)
