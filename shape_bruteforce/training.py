1d264ebc3b67822bd3a51ab22e6872d69856e049
Ahmed <ahmedkhalf567@gmail.com>
2020-07-30 16:33:42 +0400

    Add image class in training.py

--- shape_bruteforce/training.py
+++ shape_bruteforce/training.py
@@ -1,2 +1,23 @@
+import cairo
+import numpy as np
+
+
+class Image:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+        self.ctx = cairo.Context(self.surface)
+
+    def copy_image(self, img):
+        self.ctx.set_source_surface(img.surface)
+        self.ctx.paint()
+
+    def to_array(self):
+        buf = self.surface.get_data()
+        array = np.ndarray(shape=(self.width, self.height, 4), dtype=np.uint8, buffer=buf)
+        return array
+
+
 def train():
     pass
