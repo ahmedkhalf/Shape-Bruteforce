b1c38ceb0adf118ed1d0ad30b18597c35e08286e
Ahmed <ahmedkhalf567@gmail.com>
2020-08-09 20:13:26 +0400

    Add draw background method to image class

--- shape_bruteforce/training.py
+++ shape_bruteforce/training.py
@@ -9,6 +9,11 @@ class Image:
         self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
         self.ctx = cairo.Context(self.surface)
 
+    def draw_background(self, r, g, b, a=1.0):
+        self.ctx.set_source_rgba(r, g, b, a)
+        self.ctx.rectangle(0, 0, self.width, self.height)
+        self.ctx.fill()
+
     def copy_image(self, img):
         self.ctx.set_source_surface(img.surface)
         self.ctx.paint()
